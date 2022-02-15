63d27079d4a8e53d0e65f1de758758cf8d418b6f
somei <somei_tanoue@jo.hodna>
2022-02-15 21:46:03 +0900

    テストの構造化！

--- src/tests/date-only.test.ts
+++ src/tests/date-only.test.ts
@@ -1,11 +1,26 @@
 import { DateOnly } from "../lib/date-only";
 
-test(`インスタンス化できる!`, () => {
-  expect(new DateOnly(2022, 3, 11)).toBeInstanceOf(DateOnly);
-});
+describe("コンストラクタ", () => {
+  describe("正常系", () => {
+    describe.each([
+      [2022, 1, 1],
+      [2020, 2, 29],
+    ])(
+      `年月日として正常な値を渡された場合にインスタンス化できる!`,
+      (year, month, day) => {
+        test(`インスタンス化できる!${year}/${month}/${day}`, () => {
+          expect(new DateOnly(year, month, day)).toBeInstanceOf(DateOnly);
+        });
+      }
+    );
+    test(`インスタンス化できる!`, () => {
+      expect(new DateOnly(2022, 3, 11)).toBeInstanceOf(DateOnly);
+    });
+  });
 
-test(`インスタンス化できる？`, () => {
-  expect(() => {
-    new DateOnly(-1, -2, -3);
-  }).toThrowError(Error);
+  test(`インスタンス化できる？`, () => {
+    expect(() => {
+      new DateOnly(-1, -2, -3);
+    }).toThrowError(Error);
+  });
 });

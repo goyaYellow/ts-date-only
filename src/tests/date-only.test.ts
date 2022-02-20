97805590dd531e4ea40104fcdb332698847cf741
somei <somei_tanoue@jo.hodna>
2022-02-20 17:27:39 +0900

    コンストラクタのテスのバグ修正

--- src/tests/date-only.test.ts
+++ src/tests/date-only.test.ts
@@ -20,12 +20,28 @@ describe("コンストラクタ", () => {
       [-2022, 1, 1],
       [2020, -1, 1],
       [2020, 1, -1],
-      [2021, 2, 29], // うるう年じゃない
     ])(
       "年月日として異常な値が渡された場合にエラーをスローする",
       (year, month, day) => {
-        test(`インスタンス化できる!${year}/${month}/${day}`, () => {
-          () => expect(new DateOnly(year, month, day)).toThrowError(Error);
+        test(`年月日として異常な値が渡された場合にエラーをスローする${year}/${month}/${day}`, () => {
+          expect(() => {
+            new DateOnly(year, month, day);
+          }).toThrowError(Error);
+        });
+      }
+    );
+
+    describe.each([
+      [2021, 2, 29], // うるう年じゃない
+      [2100, 2, 29], // うるう年じゃない
+      [1, 1, 1],
+    ])(
+      "存在し得ない年月日が値が渡された場合にエラーをスローする",
+      (year, month, day) => {
+        test(`存在し得ない年月日が値が渡された場合にエラーをスローする。${year}/${month}/${day}`, () => {
+          expect(() => {
+            new DateOnly(year, month, day);
+          }).toThrowError(Error);
         });
       }
     );
